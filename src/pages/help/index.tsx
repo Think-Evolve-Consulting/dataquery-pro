232270062d98d701cd38554cec037e906d5b17e9
skyprince999 <aakashg80@gmail.com>
2025-02-09 03:33:05 +0530

    Updated FAQ videos

--- src/pages/help/index.tsx
+++ src/pages/help/index.tsx
@@ -12,10 +12,10 @@ const Index = () => {
         <div className="flex flex-col gap-14">
           <div className="flex flex-col gap-4">
             <h2 className="text-xl font-semibold leading-5 text-slate-700 dark:text-dark-10 lg:text-3xl">
-              How to upload file inside Data Query PRO?
+              How to map CLM data with existing inventory using Data Query PRO?
             </h2>
             <YoutubeVideo
-              url={process.env.NEXT_PUBLIC_YOUTUBE_UPLOAD_VIDEO || ''}
+              url={process.env.NEXT_PUBLIC_YOUTUBE_CLM_VIDEO || ''}
             />
           </div>
 
@@ -30,10 +30,54 @@ const Index = () => {
 
           <div className="flex flex-col gap-4">
             <h2 className="text-xl font-semibold leading-5 text-slate-700 dark:text-dark-10 lg:text-3xl">
-              Can I get reports from Data Query PRO?
+              How to upload a stock statement on DataQuery PRO?
             </h2>
             <YoutubeVideo
-              url={process.env.NEXT_PUBLIC_YOUTUBE_GET_REPORT_VIDEO || ''}
+              url={process.env.NEXT_PUBLIC_YOUTUBE_GET_STOCK_UPLOAD || ''}
+            />
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <h2 className="text-xl font-semibold leading-5 text-slate-700 dark:text-dark-10 lg:text-3xl">
+              Can I get inactive material report by uploading TECO report?
+            </h2>
+            <YoutubeVideo
+              url={process.env.NEXT_PUBLIC_YOUTUBE_GET_TECO_VIDEO || ''}
+            />
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <h2 className="text-xl font-semibold leading-5 text-slate-700 dark:text-dark-10 lg:text-3xl">
+              Can I use catalogue number to search for inventory?
+            </h2>
+            <YoutubeVideo
+              url={process.env.NEXT_PUBLIC_YOUTUBE_GET_CATCODE_VIDEO || ''}
+            />
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <h2 className="text-xl font-semibold leading-5 text-slate-700 dark:text-dark-10 lg:text-3xl">
+              How can I get insights into Purchase data?
+            </h2>
+            <YoutubeVideo
+              url={process.env.NEXT_PUBLIC_YOUTUBE_GET_PURCHASE_VIDEO || ''}
+            />
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <h2 className="text-xl font-semibold leading-5 text-slate-700 dark:text-dark-10 lg:text-3xl">
+              Can I use material name or description to search for inventory?
+            </h2>
+            <YoutubeVideo
+              url={process.env.NEXT_PUBLIC_YOUTUBE_GET_MATDESC_VIDEO || ''}
+            />
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-xl font-semibold leading-5 text-slate-700 dark:text-dark-10 lg:text-3xl">
+              Can I use material code to search for inventory?
+            </h2>
+            <YoutubeVideo
+              url={process.env.NEXT_PUBLIC_YOUTUBE_GET_MATCODE_VIDEO || ''}
             />
           </div>
         </div>
